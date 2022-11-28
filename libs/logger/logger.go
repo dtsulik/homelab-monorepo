a00aa5d0b838ff2437ca545951984897777650f7
David Tsulaia <david.tsulaia@dataart.com>
2022-11-28 15:24:22 +0400

    add basic json log

--- libs/logger/logger.go
+++ libs/logger/logger.go
@@ -1,1 +1,20 @@
 package logger
+
+import "fmt"
+
+type LogLevel string
+
+const (
+	LogLevelEmergency LogLevel = "EMERGENCY"
+	LogLevelAlert     LogLevel = "ALERT"
+	LogLevelCritical  LogLevel = "CRITICAL"
+	LogLevelError     LogLevel = "ERROR"
+	LogLevelWarning   LogLevel = "WARNING"
+	LogLevelNotice    LogLevel = "NOTICE"
+	LogLevelInfo      LogLevel = "INFO"
+	LogLevelDebug     LogLevel = "DEBUG"
+)
+
+func Log(level LogLevel, message string) {
+	fmt.Printf("{ \"level\": \"%s\", \"message\": \"%s\"", level, message)
+}
