fc35b23bdbbf1778d3537aab2b81504d15373411
David Tsulaia <dtsulik@gmail.com>
2022-12-11 14:28:16 +0400

    add apigw sekeleton

---
+++ cmd/apigw/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"net/http"
+	_ "net/http/pprof"
+
+	"gif-doggo/internal/helpers"
+	"gif-doggo/internal/jaegerexport"
+	"gif-doggo/internal/logger"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+)
+
+var tracer_name = "doggo-apigw"
+
+func main() {
+	tc_ep := helpers.GetEnv("TRACECOLLECTOR_ENDPOINT", "http://jaeger:14268/api/traces")
+	logger.Infow("Sending traces to " + tc_ep)
+
+	tp, err := jaegerexport.JaegerTraceProvider(tc_ep)
+	if err != nil {
+		logger.Fatalw("Failed to create tracer provider", "error", err)
+	}
+
+	otel.SetTracerProvider(tp)
+
+	logger.Infow("Starting server", "port", 80)
+	http.HandleFunc("/readyz", func(w http.ResponseWriter, request *http.Request) {})
+	http.HandleFunc("/livez", func(w http.ResponseWriter, request *http.Request) {})
+	http.HandleFunc("/", func(w http.ResponseWriter, request *http.Request) {
+		defer request.Body.Close()
+
+		_, span := otel.Tracer(tracer_name).Start(request.Context(), "upload")
+		defer span.End()
+
+		span.SetAttributes(attribute.String("url", request.URL.String()))
+		span.SetAttributes(attribute.String("method", request.Method))
+	})
+
+	err = http.ListenAndServe(":80", nil)
+	if err != nil {
+		logger.Fatalw("Failed to start server", "error", err)
+	}
+}
