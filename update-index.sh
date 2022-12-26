5d3ede37bf0df93ed2ab7b8b2ebb26de220501a9
David <dtsulik@gmail.com>
2022-12-26 22:32:05 +0400

    fix index

--- update-index.sh
+++ update-index.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-helm repo index charts/ --url https://dtsulik.github.io/homelab-monorepo/ --merge index.yaml
+helm repo index charts/ --url https://dtsulik.github.io/homelab-monorepo/charts --merge index.yaml
+mv charts/index.yaml .
